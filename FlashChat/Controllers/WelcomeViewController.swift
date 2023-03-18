56de5a0a9bcd765a0798e0b07edf77d961c12b3a
Mark Raitman <markraitman@mail.ru>
2023-03-18 16:25:11 +0300

    CLTypingLabel pod
    CLTypingLabel pod deleted

--- FlashChat/Controllers/WelcomeViewController.swift
+++ FlashChat/Controllers/WelcomeViewController.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 import UIKit
-import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
     
@@ -19,32 +18,32 @@ class WelcomeViewController: UIViewController {
     }
 
     //MARK: - Outlets
-    @IBOutlet weak var titleLabel: CLTypingLabel!
+    @IBOutlet weak var titleLabel: UILabel!
     
     //MARK: - Methods
     func welcomeAnimation() {
-        //Sets a text to the titleLabel w/CLTypingLabel effect.
-        titleLabel.text = "⚡️FlashChat"
+        //Sets a text to the titleLabel.
+        titleLabel.text = ""
         
-//        //Assigning the "⚡️FlashChat" string to the constant variable `titleText`.
-//        let titleText = "⚡️FlashChat"
-//
-//        /*
-//         Initializing a double variable charIndex with 0.0 value
-//         to increment by 1 for each character in the titleText and create delay.
-//        */
-//        var charIndex = 0.0
-//
-//        //Iterating through each character in the titleText using for-in loop.
-//        for letter in titleText {
-//            //Each iteration schedules a timer with a specific delay.
-//            Timer.scheduledTimer(withTimeInterval: 0.15 * charIndex, repeats: false) { (timer) in
-//                //In the timer block, append the current letter to titleLabel's text using optional chaining.
-//                self.titleLabel.text?.append(letter)
-//            }
-//            //incrementing the charIndex by 1 after each iteration.
-//            charIndex += 1
-//        }
+        //Assigning the "⚡️FlashChat" string to the constant variable `titleText`.
+        let titleText = "⚡️FlashChat"
+
+        /*
+         Initializing a double variable charIndex with 0.0 value
+         to increment by 1 for each character in the titleText and create delay.
+        */
+        var charIndex = 0.0
+
+        //Iterating through each character in the titleText using for-in loop.
+        for letter in titleText {
+            //Each iteration schedules a timer with a specific delay.
+            Timer.scheduledTimer(withTimeInterval: 0.15 * charIndex, repeats: false) { (timer) in
+                //In the timer block, append the current letter to titleLabel's text using optional chaining.
+                self.titleLabel.text?.append(letter)
+            }
+            //incrementing the charIndex by 1 after each iteration.
+            charIndex += 1
+        }
     }
 
 }
--- Podfile
+++ Podfile
@@ -3,6 +3,6 @@ platform :ios, '13.0'
 target 'FlashChat' do
   use_frameworks!
   
-pod 'CLTypingLabel'
+  
 
 end
