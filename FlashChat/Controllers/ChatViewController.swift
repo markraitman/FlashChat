802ba8e8b3f02a6c90df70c1d41a7c80aae7a125
Mark Raitman <markraitman@mail.ru>
2023-03-20 16:32:17 +0300

    TableView
    TableView setup added

--- FlashChat/Controllers/ChatViewController.swift
+++ FlashChat/Controllers/ChatViewController.swift
@@ -14,8 +14,17 @@ class ChatViewController: UIViewController {
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
         navigationBarSetup()
     }
+    
+    //MARK: - Properties
+    var messages: [Message] = [
+        Message(sender: "1@2.com", body: "Hey!"),
+        Message(sender: "a@b.com", body: "Hello!"),
+        Message(sender: "1@2.com", body: "Wassup!")
+    ]
     
     //MARK: - Outlets
     @IBOutlet weak var tableView: UITableView!
@@ -45,5 +54,21 @@ class ChatViewController: UIViewController {
         navigationController?.navigationItem.hidesBackButton = true
         title = AppName.appName
     }
+    
+}
+
+//MARK: - Extensions
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Cells.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = messages[indexPath.row].body
+        
+        return cell
+    }
+    
     
 }
---
+++ FlashChat/Views/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  FlashChat
+//
+//  Created by Марк Райтман on 20.03.2023.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
